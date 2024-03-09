745281112f78722a29aa68fc3afdc119eaf02599
Viktoria Korobjina <viktoria.korobjina@yandex.ru>
2024-03-09 18:34:39 +0300

    Corrected resolve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         PicnicFruits picnicFruits = new PicnicFruits();
-        String[] arrayFruits = picnicFruits.getWords();
-        for (int i = 0; i < arrayFruits.length; i++) {
-            System.out.println(arrayFruits[i]);
-        }
         Words words = new Words();
         System.out.println("Всего фруктов на пикнике " + words.countOfWords());
         LongestFruitsWord longestFruitsWord = new LongestFruitsWord();
